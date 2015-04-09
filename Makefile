f281f3b8887fa8fddf3fe317b7733f1bf1b025c5
Jimmy Yu <jimiyu@gmail.com>
2015-04-09 21:57:46 +0800

    add

---
+++ Makefile
@@ -0,0 +1,7 @@
+LIB=-lm
+OBJS=hello.o thanks.o
+APP_NAME=main
+main:${OBJS}
+	gcc -o ${APP_NAME} ${OBJS} ${LIBS}
+clean:
+	rm -f ${APP_NMAE} ${OBJS}
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello Jimmy!\n");
+	thanks_2();
+	float value;
+	value = sin(3.14 / 2);
+	printf("%f\n", value);
+}
