7df00cd15157606364d17d5427ebd8cc813bc346
Jimmy Yu <jimiyu@gmail.com>
2015-04-28 10:31:04 +0800

    .

--- common.h
+++ common.h
@@ -2,3 +2,9 @@
 typedef unsigned char  u8;
 typedef unsigned short u16;    
 typedef unsigned long  u32;       
+
+typedef u8 UINT8;
+typedef u16 UINT16;
+typedef u32 UINT32;
+
+#define USERINFOPS_ENTRY_SIZE               65
--- hello.c
+++ hello.c
@@ -4,18 +4,48 @@
 
 typedef struct
 {
-     u32 u32GroupType;
-     u16 u16Offset;
-     u8 u8FieldIndex;
-     u8 u8FieldNum;
+    u32 u32GroupType;
+    u32 u32Offset;
+    u8 u8FieldIndex;
+	u8 u8FieldNum;
+	u16 u16EntrySize;
 } sPSToDCMEntry;
 
-sPSToDCMEntry G_aPSToDCMTable[100] = 
+sPSToDCMEntry G_aPSToDCMTable[1000] =  {};
+
+sPSToDCMEntry G_aPSToDCMTemplate[100] = 
 {
-	{80000, 0, 1, 1},
-	{80000, 1, 2, 1},
-	{0, 0, 0, 0},
+	{80000, 0, 1, 1, 0},
+	{80000, 1, 2, 1, 0},
+	{80001, 0, 1, 16, 64},
+	{80001, 17, 2, 16, 64},
+	{0, 0, 0, 0, 0},
 };
+
+
+u16 G_u16TableIdx = 0;
+
+void AddDCMEntry(
+					UINT32 u32GroupType,
+					UINT16 u16StartOffset,
+					UINT8 u8FieldIndex,
+					UINT16 u16Num,
+					u16 u16EntrySize
+				)
+{
+	u16 i = 0;
+
+	for(i = 0; i < u16Num; i++)
+	{
+		G_aPSToDCMTable[G_u16TableIdx].u32GroupType = u32GroupType;
+		u32 u32Offset = u16StartOffset + (u16EntrySize * i);
+		G_aPSToDCMTable[G_u16TableIdx].u32Offset = u32Offset;//u16StartOffset + (u16EntrySize * i);
+		G_aPSToDCMTable[G_u16TableIdx].u8FieldIndex = u8FieldIndex;
+		G_aPSToDCMTable[G_u16TableIdx].u8FieldNum = i + 1;
+		G_aPSToDCMTable[G_u16TableIdx].u16EntrySize = u16EntrySize;
+		G_u16TableIdx++;
+	}
+}
 
 int main(void)
 {
@@ -27,27 +57,39 @@ int main(void)
 	jimmy_debug("jimmy song %f.\n", value);
 
 	int i = 0;
-	
-	u8 u8MaxLen = 17;
-
-	for(i = 0; i < 16; i++)
+	while(G_aPSToDCMTemplate[i].u32GroupType != 0)
 	{
-		int idx = i + 2;
-		G_aPSToDCMTable[idx].u32GroupType = 80001;
-		G_aPSToDCMTable[idx].u16Offset = u8MaxLen * i;
-		G_aPSToDCMTable[idx].u8FieldIndex = 2;
-		G_aPSToDCMTable[idx].u8FieldNum = i + 1;
+		jimmy_debug("GroupType:%d|u16Offset:%d|FieldIndex:%d|FieldNum:%d|EntrySize:%d.\n", 
+					G_aPSToDCMTemplate[i].u32GroupType,
+					G_aPSToDCMTemplate[i].u32Offset,
+					G_aPSToDCMTemplate[i].u8FieldIndex,
+					G_aPSToDCMTemplate[i].u8FieldNum,
+					G_aPSToDCMTemplate[i].u16EntrySize);
+		
+		AddDCMEntry(G_aPSToDCMTemplate[i].u32GroupType, 
+					G_aPSToDCMTemplate[i].u32Offset, 
+					G_aPSToDCMTemplate[i].u8FieldIndex,
+					G_aPSToDCMTemplate[i].u8FieldNum, 
+					G_aPSToDCMTemplate[i].u16EntrySize); 
+		i++;
 	}
-	
-	G_aPSToDCMTable[16 + 2].u32GroupType = 0;
-	G_aPSToDCMTable[16 + 2].u16Offset = 0;
-	G_aPSToDCMTable[16 + 2].u8FieldIndex = 0;
-	G_aPSToDCMTable[16 + 2].u8FieldNum = 0;
+
+	G_aPSToDCMTable[G_u16TableIdx].u32GroupType = 0;
+	G_aPSToDCMTable[G_u16TableIdx].u32Offset = 0;
+	G_aPSToDCMTable[G_u16TableIdx].u8FieldIndex = 0;
+	G_aPSToDCMTable[G_u16TableIdx].u8FieldNum = 0;
+	G_aPSToDCMTable[G_u16TableIdx].u16EntrySize = 0;
 
 	i = 0;
 	while(G_aPSToDCMTable[i].u32GroupType != 0)
 	{
-		jimmy_debug("%d.\n", G_aPSToDCMTable[i].u32GroupType);
+		jimmy_debug("GroupType:%d|u16Offset:%d|FieldIndex:%d|FieldNum:%d|EntrySize:%d.\n", 
+					G_aPSToDCMTable[i].u32GroupType,
+					G_aPSToDCMTable[i].u32Offset,
+					G_aPSToDCMTable[i].u8FieldIndex,
+					G_aPSToDCMTable[i].u8FieldNum,
+					G_aPSToDCMTable[i].u16EntrySize);
 		i++;
 	}
 }
+
